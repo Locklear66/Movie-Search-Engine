62761ea6455e6dc7a5bb9acdb5481b3b52873555
Lance Wooley <lancewooley@Lances-MacBook-Pro.local>
2023-03-13 20:57:57 -0500

    added the contact page

---
+++ contact/contact.js
@@ -0,0 +1,24 @@
+function openMenu() {
+    document.body.classList += " menu--open"
+ }
+ 
+ function closeMenu() {
+    document.body.classList.remove('menu--open')
+ }
+ 
+ function contact(event) {
+    event.preventDefault()
+    emailjs
+       .sendForm(
+          'service_uc6u3wm',
+          'template_bpavk3e',
+          event.target,
+          'rJeubM889V0KM5C2W'
+ 
+       ).then(() => {
+          console.log('this worked')
+       })
+       setTimeout(function(){
+          location.reload();
+        }, 2000);
+ }
